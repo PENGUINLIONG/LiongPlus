f63978071df02a0038eb15f40b531d160487c9c5
Rendong Liang <penguinliong@sina.com>
2016-01-19 20:14:52 +0800

    IProgress(T)

---
+++ Include/IProgress.hpp
@@ -0,0 +1,19 @@
+// File: IProgress.hpp
+// Author: Rendong Liang (Liong)
+
+#ifndef _L_IProgress
+#define _L_IProgress
+#include "Fundamental.hpp"
+
+namespace LiongPlus
+{   
+    template<typename T>
+    class IProgress
+        : public Interface
+    {
+    public:
+        virtual void Report(T value) = 0;
+    }
+}
+
+#endif
